85fd7cb5b7aecb76191ffd0244c59f549ccfe2ca
Abhinay Kumar Nathani <anathani@my.bridgeport.edu>
2023-11-16 02:23:13 -0500

    text update

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "I am glad to have completed this tutorial.",
+            "message": "hello world",
             # "location": ip.text.replace("\n", "")
         }),
     }
